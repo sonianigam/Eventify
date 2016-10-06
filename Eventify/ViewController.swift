1a89793c27bf58da7643faa6f0f1b29110cec310
Sonia Nigam <sonianigam@sbcglobal.net>
2016-10-06 16:02:42 -0500

    iOS app built at hackatho

---
+++ Eventify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eventify
+//
+//  Created by sonia on 11/21/15.
+//  Copyright (c) 2015 Sonia Nigam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
